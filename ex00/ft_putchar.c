8d627898c214b5d9d12fad5f44891079e11d7796
Ekaitz López <ekaitzlopez2025@gmail.com>
2026-01-09 14:47:49 +0100

    Update ex00

--- ex00/ft_putchar.c
+++ ex00/ft_putchar.c
@@ -5,8 +5,7 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-/*
-int	main(void)
+/*int	main(void)
 {
 	ft_putchar('H');
 	ft_putchar('e');
@@ -22,5 +21,4 @@ int	main(void)
 	ft_putchar('d');
 	ft_putchar('!');
 	ft_putchar('\n');
-}
-*/
+}*/
