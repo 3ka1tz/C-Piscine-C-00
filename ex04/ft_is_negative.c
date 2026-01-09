c66cd68a5fec29fce1d7c310d43155b2c574cd45
Ekaitz López <ekaitzlopez2025@gmail.com>
2026-01-09 14:53:53 +0100

    Update

--- ex04/ft_is_negative.c
+++ ex04/ft_is_negative.c
@@ -8,20 +8,18 @@ void	ft_is_negative(int n)
 		write(1, "P", 1);
 }
 
-/*
-int	main(void)
+/*int	main(void)
 {
 	write(1, "ft_is_negative(-1) = ", 21);
 	ft_is_negative(-1);
 	write(1, "\n", 1);
-	write(1, "ft_is_negative(0)  = ", 21);
+	write(1, "ft_is_negative(0) = ", 20);
 	ft_is_negative(0);
 	write(1, "\n", 1);
-	write(1, "ft_is_negative(1)  = ", 21);
+	write(1, "ft_is_negative(1) = ", 20);
 	ft_is_negative(1);
 	write(1, "\n", 1);
 	write(1, "ft_is_negative(+1) = ", 21);
 	ft_is_negative(+1);
 	write(1, "\n", 1);
-}
-*/
+}*/
